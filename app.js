b80c4a47e280ebdd83e176bb347665a9535efefd
lyhcode (Yan-hong Lin) <lyhcode@gmail.com>
2012-06-11 13:10:04 +0800

    增加 /logout 功能

--- app.js
+++ app.js
@@ -61,10 +61,11 @@ db.open(function(err, nativedb) {
     app.get('/', routes.index);
     app.get('/chrome.:format?', routes.chrome); //google chrome frame
     app.get('/login.:format?', routes.login);
+    app.get('/logout.:format?', routes.logout);
     app.get('/portal.:format?', routes.portal);
     app.get('/select.:format?', routes.select);
     app.get('/realtime.:format?', routes.realtime);
-	app.get('/admin.:format?', routes.admin); //課務組手動加選網站(無管控人數上限)
+    app.get('/admin.:format?', routes.admin); //課務組手動加選網站(無管控人數上限)
 
     // Load Services
     require('./service')(app, db, urlprefix);
--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,55 @@
 exports.index = function(req, res){
-	res.redirect('/login');
+    res.redirect('/login');
 };
 
 exports.chrome = function(req, res){
-	res.render('chrome.jade', { title: '安裝 Google Chrome Frame', bootstrap: '', siteurl: req.headers.host });
+    res.render('chrome.jade', { title: '安裝 Google Chrome Frame', bootstrap: '', siteurl: req.headers.host });
 };
 
 exports.login = function(req, res){
-	// clear sessions and cookies
-	req.session.destroy();
-	res.clearCookie('JSESSIONID');
-	res.clearCookie('jsessionid');
+    // clear sessions and cookies
+    req.session.destroy();
+    res.clearCookie('JSESSIONID');
+    res.clearCookie('jsessionid');
 
-	res.render('login.jade', { title: '登入選課系統', bootstrap: 'login.js' });
+    res.render('login.jade', { title: '登入選課系統', bootstrap: 'login.js' });
 }
 
 exports.portal = function(req, res){
-	if (req.session.user) {
-		res.render('portal.jade', { title: '大仁科技大學選課系統', bootstrap: 'portal.js' });
-	}
-	else {
-		res.redirect('login');
-	}
+    if (req.session.user) {
+        res.render('portal.jade', { title: '大仁科技大學選課系統', bootstrap: 'portal.js' });
+    }
+    else {
+        res.redirect('login');
+    }
 }
 
 exports.select = function(req, res){
-	res.redirect('/login?mode=select');
+    res.redirect('/login?mode=select');
 };
 
 exports.realtime = function(req, res){
-	res.redirect('/login?mode=realtime');
+    res.redirect('/login?mode=realtime');
 };
 
 //課務組手動加選網站(無管控人數上限)
 exports.admin = function(req, res){
-	res.redirect('/login?mode=realtime&admin=admin');
+    res.redirect('/login?mode=realtime&admin=admin');
+};
+
+//登出
+exports.logout = function(req, res){
+    //清除 SESSION
+    req.session.user = null;
+
+    //重導至指定的 redirect
+    var redirect_url = req.query.redirect;
+
+    console.log(redirect_url);
+    if (redirect_url) {
+        res.redirect(redirect_url);
+    }
+    else {
+        res.redirect('/login');
+    }
 };
